8da7d3057dc33a2f5e45eb1973685784bf49bdce
david <dp_chua@hotmail.com>
2021-03-25 01:52:26 -0400

    Instagram clone - working search and like/dislike

---
+++ WebApplications1/IntroToReact/React-Components-Insta-Clone-main/src/components/Posts/Posts.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Post from './Post';
+import './Posts.css';
+
+const Posts = (props) => {
+  // 🔥 Make sure the parent of Posts is passing the right props!
+  const { likePost, posts, likeByUser } = props;
+
+  return (
+    <div className='posts-container-wrapper'>
+      {/* Map through the posts array returning a Post component at each iteration */}
+      {/* Check the implementation of Post to see what props it requires! */}
+      {posts.map(post => {
+        return <Post post={post} likeByUser={likeByUser} likePost={likePost}/>
+      })}
+    </div>
+  );
+};
+
+export default Posts;
