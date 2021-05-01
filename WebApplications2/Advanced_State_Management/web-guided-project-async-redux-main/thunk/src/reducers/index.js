cb86b14d2e4bbcd98b1c512c1d9993ad160c7aca
david-chua <dp_chua@hotmail.com>
2021-05-01 02:33:28 -0400

    Web Guided Async Redux start

---
+++ WebApplications2/Advanced_State_Management/web-guided-project-async-redux-main/thunk/src/reducers/index.js
@@ -0,0 +1,13 @@
+
+const initialState = {
+  quote: null,
+  isFetching: false,
+  error: ''
+};
+
+export const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
