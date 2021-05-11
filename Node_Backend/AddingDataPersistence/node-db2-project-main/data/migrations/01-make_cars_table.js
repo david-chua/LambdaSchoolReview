cc04abb3505bd3bd044005d243aa0be907c3469a
david-chua <dp_chua@hotmail.com>
2021-05-11 01:32:48 -0400

    project - database and seeds created.

--- Node_Backend/AddingDataPersistence/node-db2-project-main/data/migrations/01-make_cars_table.js
+++ Node_Backend/AddingDataPersistence/node-db2-project-main/data/migrations/01-make_cars_table.js
@@ -1,1 +1,16 @@
 // DO YOUR MAGIC
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable("cars", tbl => {
+    tbl.increments();
+    tbl.string('vin').notNullable().unique();
+    tbl.string('make').notNullable();
+    tbl.string('model').notNullable();
+    tbl.float('mileage').notNullable();
+    tbl.string('title');
+    tbl.string('transmission');
+  })
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists("cars");
+};
--- Node_Backend/AddingDataPersistence/node-db2-project-main/data/seeds/01-cars.js
+++ Node_Backend/AddingDataPersistence/node-db2-project-main/data/seeds/01-cars.js
@@ -1,1 +1,15 @@
 // STRETCH
+exports.seed = function(knex) {
+  // Deletes ALL existing entries
+  return knex('cars').truncate()
+    .then(function () {
+      // Inserts seed entries
+      return knex('cars').insert([
+        { vin: "dadvbndagd5dsdga", make: 'Toyota', model:'Corolla', mileage: 123455.3, title: 'carTitle1', transmission: 'automatic' },
+        { vin: "adsgfagda235435d", make: 'Honda', model: 'Civic', mileage: 20224.5, title: 'carTitle2', transmission: 'automatic' },
+        { vin: "dagd2535cdbdig35", make: 'Subaru', model: 'BRZ', mileage: 1200.2, title: 'carTitle3', transmission: 'manual' },
+        { vin: "adcgd25356edasg35", make: 'Toyota', model: 'Prius', mileage: 50245, title: 'carTitle4', transmission: 'automatic' },
+        { vin: "dkgibvdkantg;k235", make: 'Tesla' ,model: 'Model S', mileage: 15000.32, title: 'carTitle5', transmission: 'automatic'}
+      ]);
+    });
+};
