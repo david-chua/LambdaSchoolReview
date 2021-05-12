feadb3deccd937f545024e84bbfe23c4de214bad
david-chua <dp_chua@hotmail.com>
2021-05-12 02:52:03 -0400

    Finished guided lecture 4, need to do guided lecture 3

---
+++ Node_Backend/AddingDataPersistence/node-db4-guided-main/data/migrations/20210512062550_first-migration.js
@@ -0,0 +1,52 @@
+
+exports.up = function(knex) {
+  return knex.schema
+  .createTable("zoos", tbl => {
+    tbl.increments("zoo_id")
+    tbl.string("zoo_name", 128).notNullable()
+    tbl.string("address", 128).notNullable().unique()
+  })
+
+  .createTable("species", tbl => {
+    tbl.increments("species_id")
+    tbl.string("species_name",128).notNullable().unique()
+  })
+
+  .createTable("animals", tbl => {
+    tbl.increments("animal_id")
+    tbl.string("animal_name", 128).notNullable()
+    tbl.integer("species_id")
+      .unsigned()
+      .notNullable()
+      .references("species_id")  // foreign key
+      .inTable("species")
+      .onDelete("CASCADE")  // RESTRICT OR CASCADE
+  })
+
+  .createTable("zoo_animals", tbl => {
+    tbl.increments("zoo_animals_id")
+
+    tbl.integer("zoo_id")
+      .unsigned()
+      .notNullable()
+      .references("zoo_id")
+      .inTable("zoos")
+      .onDelete("CASCADE")
+
+    tbl.integer("animal_id")
+      .unsigned()
+      .notNullable()
+      .references("animal_id")
+      .inTable("animals")
+      .onDelete("CASCADE")
+  })
+
+};
+
+exports.down = function(knex) {
+    return knex.schema
+    .dropTableIfExists("zoo_animals")
+    .dropTableIfExists("animals")
+    .dropTableIfExists("species")
+    .dropTableIfExists("zoos")
+};
