3db95076450bff8f00cbadb1655e568cf3c99c61
david-chua <dp_chua@hotmail.com>
2021-05-27 21:19:05 -0400

    challenge 6

--- ComputerScience/Computer_Science_Fundamentals/cs-guided-project-python-ii-master/src/demonstration_06.py
+++ ComputerScience/Computer_Science_Fundamentals/cs-guided-project-python-ii-master/src/demonstration_06.py
@@ -7,6 +7,23 @@ We will consider `a, e, i, o, u as vowels for this challenge (but not y).
 
 The input string will only consist of lower case letters and/or spaces.
 """
+# def get_count(input_str):
+#     # Your code here
+#     amount = 0
+#     for char in input_str:
+#         if char in 'aeiouAEIOU':
+#             amount += 1
+#     return amount
+
 def get_count(input_str):
     # Your code here
+    amount = 0
+    for char in input_str:
+        if char.lower() in 'aeiou':
+            amount += 1
+    return amount
 
+sample_str = 'auidah kEoLmno'
+
+
+print(get_count(sample_str))
