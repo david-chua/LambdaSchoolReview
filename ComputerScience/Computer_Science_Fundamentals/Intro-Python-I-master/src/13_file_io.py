34f54179e0be9b7e124b4648c0335e753bc960e5
david <dp_chua@hotmail.com>
2021-05-29 01:09:56 -0400

    coding

--- ComputerScience/Computer_Science_Fundamentals/Intro-Python-I-master/src/13_file_io.py
+++ ComputerScience/Computer_Science_Fundamentals/Intro-Python-I-master/src/13_file_io.py
@@ -10,6 +10,8 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Note: pay close attention to your current directory when trying to open "foo.txt"
 
 # YOUR CODE HERE
+f = open('foo.txt', 'r')
+print(f.read())
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
@@ -17,3 +19,10 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+b = open('bar.txt', 'w')
+b.write('This is a line\n')
+b.write('This is a second line\n')
+b.write('This is a third line\n')
+b.close()
+b = open('bar.txt', 'r')
+print(b.read())
