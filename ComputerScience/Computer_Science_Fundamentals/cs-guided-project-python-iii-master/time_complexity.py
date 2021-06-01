2715c9df8d9242606c096d2c5813eeccba2557bb
david <dp_chua@hotmail.com>
2021-06-01 02:35:14 -0400

    constant time complexity

--- ComputerScience/Computer_Science_Fundamentals/cs-guided-project-python-iii-master/time_complexity.py
+++ ComputerScience/Computer_Science_Fundamentals/cs-guided-project-python-iii-master/time_complexity.py
@@ -1,10 +1,14 @@
+# Constant O(1)- takes one step. 
+def print_one_item(items):
+    print(item[0])
 
-# Linear - goes through every item once
+
+# Linear O(n) - goes through every item once
 def print_every_item(items):
     for item in items:
         print(item)
 
-# Exponential 
+# Exponential - Quardratic O(n^2)
 def print_pairs(items):
     for item_one in items:
         for item_two in items:
