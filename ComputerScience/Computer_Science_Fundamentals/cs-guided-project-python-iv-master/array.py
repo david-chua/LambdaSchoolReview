755482f6a607b870955d386ecd2e364220c1eeab
david-chua <dp_chua@hotmail.com>
2021-06-03 00:10:08 -0400

    pivot index

---
+++ ComputerScience/Computer_Science_Fundamentals/cs-guided-project-python-iv-master/array.py
@@ -0,0 +1,16 @@
+
+arr = [1,2,3,4]
+
+# Access is O(1) - it's just an addition operation
+print(arr[2])
+print(arr[0])
+
+
+# for static arrays
+# O(n)
+arr.append(5)
+
+
+# for dynamic arrays
+O(1) - typically
+arr.append(6)
