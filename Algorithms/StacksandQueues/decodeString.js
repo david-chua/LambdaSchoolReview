fd467d703ac8dd8f434a2412ac0d61b75cd7f008
david-chua <dp_chua@hotmail.com>
2021-06-11 00:53:14 -0400

    decode string question

--- Algorithms/StacksandQueues/decodeString.js
+++ Algorithms/StacksandQueues/decodeString.js
@@ -1,3 +1,33 @@
+/**
+Decode String
+
+Given an encoded string , return its decoded string.
+
+The encoding rule is k[encoded_string], where encoded_string inside the square brackets is being repeated exactly k times. Note that k is guaranteed to be a positive integer.
+
+You may assume that the input string is always valid. No extra white spaces, square brackets are well-formed, etc.
+
+Furthermore, you may assume that the original data does not contain any digits and that digits are only for those repeat numbers, k.
+
+Examples
+
+sample 1:
+Input s = "3[a]2[bc]"
+output: aaabcbc
+
+sample 2:
+input: s = "3[a2[c]]"
+output: "accaccacc"
+
+sample 3:
+input s = "2[abc]3[cd]ef"
+output: abcabccdcdcdef
+
+sample 4:
+input s = "abc3[cd]xyz"
+output = abccdcdcdxyz
+**/
+
 var decodeString = function(s) {
     let numStack = []
     let strStack = []
