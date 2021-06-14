f429a85a895a5540510c3140f8b1c7fd3deed43b
david-chua <dp_chua@hotmail.com>
2021-06-14 01:22:55 -0400

    missing number questions

--- Algorithms/Strings&Arrays/MissingNumber.js
+++ Algorithms/Strings&Arrays/MissingNumber.js
@@ -27,6 +27,8 @@ Input: nums = [0]
 Output: 1
 Explanation: n = 1, since there is 1 number, so all numbers are in range of [0,1]. 1 is the missing number.
 
+**/
+
 var missingNumber = function(nums) {
     let sum = addNum(nums.length);
     for (let numIndex = 0; numIndex < nums.length; numIndex++){
