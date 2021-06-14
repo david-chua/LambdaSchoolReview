364e49fd823c2d06fe32c8073d312e42ac9b673d
david-chua <dp_chua@hotmail.com>
2021-06-13 23:16:29 -0400

    Writing question

--- Algorithms/Strings&Arrays/RomanToInteger.js
+++ Algorithms/Strings&Arrays/RomanToInteger.js
@@ -1,5 +1,18 @@
+/**
+Roman numerals are represented by seven different symbols
+I,V,X,L,C,D, and M.
 
+For example 2 is written as II. 12 is written as XII which is simply X + II.
 
+Roman numerals are typically written largest to smallest from left to write.
+However, the numeral 4 is not IIII, instead it's IV.
+
+I can be placed before V(5) and X(10) to make 4 and 9.
+X can be placed before L(50) and C(100) to make 40 and 90
+C can be placed before D(500) and M(1000) to make 400 and 900.
+
+Given a roman numeral, convert it to an integer.
+**/
 
 
 // while loop
