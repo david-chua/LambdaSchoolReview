3c6a05c4244033e60c48fd89098c26d2069c1707
david-chua <dp_chua@hotmail.com>
2021-06-15 00:38:15 -0400

    Merge two sorted list

--- Algorithms/LinkedList/MergeTwoSwortedLists.js
+++ Algorithms/LinkedList/MergeTwoSwortedLists.js
@@ -17,6 +17,15 @@ Input:
 l1 = [], l2= [0]
 Ouptut: [0]
 
+/** solution
+1. create a dummy node which will act as a fake head.
+2. add a second pointer to keep a head pointer since we'll be moving dummy around the two lists.
+3. while loop where both list are not null.
+4. inside while loop check if l1 is less than l2's value. if it is, dummy.next = l1 and then move the l1 value to the next value. if it is greater than l2, change dummy.next to be l2's value and move l2 to the next value. At the end, move dummy to point to its next value so you can keep the linkedlist moving.
+
+5. If while loop breaks and l1 is still not null, move the next value to the current l1 value. If l2 is not null, move the next value to the current l2 value.
+6. return head's next value, which is pointing to the first value used between l1 and l2. 
+
 /**
  * Definition for singly-linked list.
  * function ListNode(val, next) {
