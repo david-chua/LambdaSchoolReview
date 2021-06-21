cef82826353de3eef2124ea553cf65cbf20d0c83
david-chua <dp_chua@hotmail.com>
2021-06-21 00:43:09 -0400

    Palindrome

--- Algorithms/LinkedList/PalindromeLinkedList.js
+++ Algorithms/LinkedList/PalindromeLinkedList.js
@@ -7,7 +7,7 @@ Check if the linked list is palindrome.
 var isPalindrome = function(head){
   let fast = head;
   let slow = head;
-  while(fast !== null & fast.next !== null){
+  while(fast !== null && fast.next !== null){
     slow = slow.next;
     fast = fast.next.next;
   }
@@ -25,7 +25,7 @@ var isPalindrome = function(head){
     fast = fast.next
   }
 
-  return true; 
+  return true;
 }
 
 
