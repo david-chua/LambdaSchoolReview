c59f8899fe1528093268f05c9ef5b8251b524cd0
david-chua <dp_chua@hotmail.com>
2021-07-04 22:51:03 -0400

    practicing code

---
+++ Algorithms/JavaScriptFunctions/flattenArray.js
@@ -0,0 +1,28 @@
+
+
+function flattenArray(arr){
+  let finalArray = [];
+  for (let i = 0; i < arr.length; i++){
+    if (Array.isArray(arr[i])){
+      flattenHelper(arr[i], finalArray);
+    } else {
+      finalArray.push(arr[i])
+    }
+  }
+  return finalArray;
+}
+
+function flattenHelper(arr, finArr){
+  for (let i = 0; i < arr.length; i++){
+    if (Array.isArray(arr[i])){
+      flattenHelper(arr[i], finArr)
+    } else {
+      finArr.push(arr[i])
+    }
+  }
+}
+
+
+let testArr = [1,2,[3,4], [2,[3,4]]];
+
+console.log(flattenArray(testArr))
